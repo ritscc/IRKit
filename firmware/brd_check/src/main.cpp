62a0555ec54d8969f9d9b93018788e6320ffbce8
kouichi <kanon.amnos24@gmail.com>
2019-09-08 04:16:06 +0900

    Check Wi-Fi

--- firmware/brd_check/src/main.cpp
+++ firmware/brd_check/src/main.cpp
@@ -1,15 +1,53 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const int BAUDRATE = 115200;
+const char* ssid = "SSID";
+const char* password = "PASS";
 
 const int GREEN_LED = 33;
 const int USER_SW = 32;
+const int IR_RECEIVE = 17; 
 
 void setup() {
+  Serial.begin(BAUDRATE);
+  Serial.println();
+  Serial.println("----------------------------");
+  Serial.println("KC3-IRKit-Board Test Program");
+  Serial.println("----------------------------");
+  
   // Setup GPIO pin mode
   pinMode(USER_SW, INPUT_PULLUP);
   pinMode(GREEN_LED, OUTPUT);
+  pinMode(IR_RECEIVE, INPUT);
+
+  // Connect Wi-Fi
+  connectWiFi();
+
+  // GREEN_LED ON
+  digitalWrite(GREEN_LED, LOW);
 }
 
 void loop() {
-  digitalWrite(GREEN_LED, digitalRead(USER_SW));
-  delay(10);
+  digitalWrite(GREEN_LED, !digitalRead(IR_RECEIVE));
+}
+
+/**
+ * Connect WiFi
+ */
+void connectWiFi()
+{
+    WiFi.begin(ssid, password);
+    Serial.print("WiFi connecting...");
+    while(WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        digitalWrite(GREEN_LED, HIGH);
+        delay(100);
+        Serial.print(".");
+        digitalWrite(GREEN_LED, LOW);
+        delay(100);
+    }
+    Serial.println("\nConnected.");
+    Serial.print("IP ADDRESS: ");
+    Serial.println(WiFi.localIP());
 }
